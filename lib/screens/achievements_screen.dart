15588f0b47854cbc8e32f7222eaabbed8e4ae6a8
Denzil Muchangi <njagidenzil@gmail.com>
2025-10-22 22:51:32 +0300

    refactor(achievements_screen): remove unused profile import

--- lib/screens/achievements_screen.dart
+++ lib/screens/achievements_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../models/profile.dart';
 import '../models/achievement.dart';
 import '../providers/profile_provider.dart';
 
